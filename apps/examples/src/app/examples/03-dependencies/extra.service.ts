dba45397bfea7270e8c39a5c4fc7f646c8ce3126
Lukas Ruebbelke <lukas.ruebbelke@briebug.com>
2020-08-26 19:44:17 -0700

    update: broken test runner

--- apps/examples/src/app/examples/03-dependencies/extra.service.ts
+++ apps/examples/src/app/examples/03-dependencies/extra.service.ts
@@ -1,7 +1,10 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ExtraService {
+  constructor(private http: HttpClient) {}
+
   getExtra() {
     return 'Eeeeeeeeeeeeeextra!';
   }
